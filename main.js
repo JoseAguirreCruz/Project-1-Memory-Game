e771f6202e18039b3777e6388391db272281ee5a
Jose Aguirre <aguirrejose615@gmail.com>
2023-03-07 10:25:50 -0500

    added face Arr

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const boardEl = document.getElementById('board');
-const squares = Array.from(document.getElementsByClassName('square'));
+const squares = Array.from(document.getElementsByClassName('squareF'));
 const messageEl = document.querySelector('h1');
+const faceCard = Array.from(document.getElementsByClassName('cardFaces'));
+
 
 
 
@@ -13,4 +15,22 @@ const players = {
   1: 'player1',
   '-1': 'playuer2'
 }
-console.log(players)
+
+// Event listeners
+//ex: someEl.addEventListener('click', handledrop)
+
+// functions
+
+init();
+
+function init() {
+  board = [
+    [0, 0, 0, 0, 0]//0
+    [0, 0, 0, 0, 0]//1
+  ]
+  turn = 1
+  winner = null
+  render()
+}
+
+const shuffleCards = board.sort((Arr, idx) => Math.random() - 0.5)
