2dc26c07c18660cec7b7aec596f6e027f1451dc8
Jose Aguirre <aguirrejose615@gmail.com>
2023-03-07 14:56:19 -0500

    added img/clikcevt/functions

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const boardEl = document.getElementById('board');
-const squares = Array.from(document.getElementsByClassName('squareF'));
-const messageEl = document.querySelector('h1');
-const faceCard = Array.from(document.getElementsByClassName('cardFaces'));
-
-
-
+const boardEl = document.getElementById('board')
+const squaresElF = Array.from(document.getElementsByClassName('squareF'))
+const squaresElB = Array.from(document.getElementsByClassName('squareB'))
+const messageEl = document.querySelector('h1')
+const faceCard = Array.from(document.getElementsByClassName('cardFaces'))
 
 let board
 let turn
@@ -21,16 +19,68 @@ const players = {
 
 // functions
 
-init();
+init()
 
 function init() {
   board = [
-    [0, 0, 0, 0, 0]//0
-    [0, 0, 0, 0, 0]//1
+    [0, 0, 0, 0, 0][(0, 0, 0, 0, 0)] //0 //1
   ]
   turn = 1
   winner = null
   render()
 }
 
-const shuffleCards = board.sort((Arr, idx) => Math.random() - 0.5)
+function render() {
+  renderBoard()
+  renderMessage()
+}
+
+// const shuffleCards = board.sort((Arr, idx) => Math.random() - 0.5)
+document.querySelectorAll('#board').forEach(function (board) {
+  board.addEventListener('click', function () {
+    this.querySelector('.cardFaces').classList.toggle('flipped')
+  })
+})
+
+;[...faceCard].forEach((cardArr) => {
+  cardArr.addEventListener('click', function () {
+    cardArr.classList.toggle('is-flipped')
+  })
+})
+
+//flipCardBack
+function flipCard() {
+  setTimeout(function () {
+    squaresElF.classList.remove('squareF')
+    squaresElF.classList.remove('squareB')
+  }, 3000)
+  flipCardFront()
+}
+
+function renderBoard() {
+  board.forEach(function (pArr, pIdx) {
+    if (Array.isArray(pArr)) {
+      pArr.forEach(function(Arr, idx) {
+        const squareId = `${pIdx}${idx}`
+        const squareEl = document.getElementById(squareId)
+        const cardImg = document.createElement('img')
+        cardImg.src = 'card_back.png'
+        squareEl.appendChild(cardImg)
+      })
+    }
+  })
+}
+
+
+
+function renderMessage() {
+  if (winner === 'T') {
+    messageEl.textContent = `It's a draw!`
+  } else if (winner) {
+    let currentPlayer = players[winner]
+    messageEl.innerHTML = `<span style="color: ${currentPlayer};">${currentPlayer.toUpperCase()}</span> Wins!`
+  } else {
+    let currentPlayer = players[turn]
+    messageEl.innerHTML = `<span style="color: ${currentPlayer};">${currentPlayer.toUpperCase()}</span>'s Turn...`
+  }
+}
